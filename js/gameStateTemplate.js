c0c347a0be1b090e149c95e0055505e6e9560aba
nobody190 <50930043+nobody190@users.noreply.github.com>
2020-04-25 22:42:32 +0100

    Criados directórios para uma melhor organização

---
+++ js/gameStateTemplate.js
@@ -0,0 +1,20 @@
+//Aqui vai estar como deviamos criar os estados do jogo(menu, níveis, etc)
+
+class StateName extends Phaser.State {
+    //vai buscar as imagens e sons que vão ser usados
+	preload() {
+		this.load.image('sky','sources/sky.png');
+
+	}
+
+    //cria os objectos (plataformas, jogador,etc)
+	create() {
+        //...
+	}
+
+    //corre ciclicamente
+	update() {
+        //...
+	}
+
+}

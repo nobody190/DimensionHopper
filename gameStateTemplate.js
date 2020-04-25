c29661db22c43263ae82c7f4cdc1292017bb3000
Hiers <user@user.lan>
2020-04-25 11:13:52 +0100

    Adicionei o template do esqueleto que estou a pensar usar para criar os níveis e o jogador.

---
+++ gameStateTemplate.js
@@ -0,0 +1,19 @@
+//Aqui vai estar como deviamos criar os estados do jogo(menu, níveis, etc)
+
+class StateName extends Phaser.State {
+    //vai buscar as imagens e sons que vão ser usados
+	preload() {
+		//...
+	}
+
+    //cria os objectos (plataformas, jogador,etc)
+	create() {
+        //...
+	}
+
+    //corre ciclicamente
+	update() {
+        //...
+	}
+
+}

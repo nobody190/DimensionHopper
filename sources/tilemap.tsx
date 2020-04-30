59b242759d2b0aa460b5395067071c2956c8f39f
Hiers <user@user.lan>
2020-04-30 11:13:07 +0100

    Reversão dos últimos commits.

--- sources/tilemap.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.3.4" name="tilemap" tilewidth="20" tileheight="20" tilecount="100" columns="25">
- <image source="tilemap.png" trans="ff00ff" width="504" height="84"/>
-</tileset>
